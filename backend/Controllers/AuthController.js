66066f8a08ac42719bfcf27b8e0d236f368681fe
Amit Kumar <ak.amit9041@gmail.com>
2026-09-24 22:21:59 +0530

    changes in env and frontend file

--- backend/Controllers/AuthController.js
+++ backend/Controllers/AuthController.js
@@ -32,22 +32,28 @@ const signup = async (req, res) => {
 const login = async (req, res) => {
     try {
         const { email, password } = req.body;
+       
         const user = await UserModel.findOne({ email });
         const errorMsg = 'Auth failed email or password is wrong';
         if (!user) {
             return res.status(403)
                 .json({ message: errorMsg, success: false });
         }
+
+        
         const isPassEqual = await bcrypt.compare(password, user.password);
         if (!isPassEqual) {
             return res.status(403)
                 .json({ message: errorMsg, success: false });
         }
+
+        
         const jwtToken = jwt.sign(
             { email: user.email, _id: user._id },
             process.env.JWT_SECRET,
             { expiresIn: '24h' }
         )
+        
 
         res.status(200)
             .json({
@@ -60,7 +66,7 @@ const login = async (req, res) => {
     } catch (err) {
         res.status(500)
             .json({
-                message: "Internal server errror",
+                message: err,
                 success: false
             })
     }
